265484dff9238e207c6d5e9b626b599b8841bfcf
Ahmad Fauzan <fauzan@keyta.id>
2021-10-29 22:03:05 +0700

    fix pagination

--- app/commands/services/restaurant_service.rb
+++ app/commands/services/restaurant_service.rb
@@ -6,15 +6,14 @@ module Services
             q = BusinessHour.where("business_hours.day = ?", day)
                 .where("business_hours.open_time <= ?", time)
                 .where("business_hours.close_time >= ?", time)
-                .ransack(params[:q])
 
-            bussiness_hours = q.result.paginate(:page => params[:page], :per_page => params[:per_page])
+            bussiness_hours = q.paginate(page: params[:page], per_page: params[:per_page])
             
             data = {
-                total: q.result.count, 
-                current_page: bussiness_hours.current_page, 
-                total_pages:(q.result.count/params[:per_page].to_i)+1, 
-                limit: bussiness_hours.per_page,
+                total: q.length, 
+                current_page: params[:page], 
+                total_pages:(q.length/params[:per_page].to_i)+1, 
+                limit: params[:per_page],
                 datetime: params[:datetime],
                 bussiness_hours: bussiness_hours.as_json(
                     :include => [
@@ -32,7 +31,7 @@ module Services
             location = params[:location].split(",")
             q = Restaurant.near(location, params[:distance])
 
-            restaurants = q.limit(params[:per_page]).paginate(:page => params[:page])
+            restaurants = q.paginate(page: params[:page], per_page: params[:per_page])
 
             data = {
                 total: q.length, 
@@ -43,5 +42,5 @@ module Services
             }
             Handler::Res.call(200, "Success retrive data", data)
         end
-    
+    end
 end
